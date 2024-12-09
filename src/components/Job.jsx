7fef887546eaf4cf37dc3973e146f2c3c559b5d4
notdav04 <davide.alonzi26@gmail.com>
2024-12-09 16:06:32 +0100

    first commit

---
+++ src/components/Job.jsx
@@ -0,0 +1,36 @@
+import { Row, Col, Button } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+
+const Job = ({ data }) => {
+  const dispatch = useDispatch();
+  return (
+    <Row
+      className="mx-0 mt-3 p-3"
+      style={{ border: "1px solid #00000033", borderRadius: 4 }}
+    >
+      <Col xs={3}>
+        <Link to={`/${data.company_name}`}>{data.company_name}</Link>
+      </Col>
+      <Col xs={3}>
+        <Button
+          onClick={() => {
+            dispatch({
+              type: "ADD_FAV",
+              payload: data.company_name
+            });
+          }}
+        >
+          Add to favourites
+        </Button>
+      </Col>
+      <Col xs={6}>
+        <a href={data.url} target="_blank" rel="noreferrer">
+          {data.title}
+        </a>
+      </Col>
+    </Row>
+  );
+};
+
+export default Job;
